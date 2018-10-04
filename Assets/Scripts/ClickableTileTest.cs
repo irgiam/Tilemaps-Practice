d27976353ad80c76dd9213de7420564828331e00
irgiam <imuqsit@gmail.com>
2018-10-04 11:07:35 +0700

    pake laptop pertama kali
    +tmbahan bbrp note di clickabletiletest

--- Assets/Scripts/ClickableTileTest.cs
+++ Assets/Scripts/ClickableTileTest.cs
@@ -4,7 +4,9 @@ using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class ClickableTileTest : MonoBehaviour {
-
+    /*1. buat grid system pake tile method (kaya di script mygridsystem)
+      2. grid system tadi di buat dalam/digabungkan ke fungsi grid manual yg dibuat
+      3. dalam gabungan tadi, grid tile method dtambah 0.5f  */
     // Use this for initialization
     void Start()
     {
