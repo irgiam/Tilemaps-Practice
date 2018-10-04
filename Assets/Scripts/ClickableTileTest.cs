3b848f7476d826e65245e0afc85b3f2e13ab46a6
irgiam <imuqsit@gmail.com>
2018-10-04 11:43:55 +0700

    pake laptop
    blom ditambahin 0.5

--- Assets/Scripts/ClickableTileTest.cs
+++ Assets/Scripts/ClickableTileTest.cs
@@ -7,16 +7,29 @@ public class ClickableTileTest : MonoBehaviour {
     /*1. buat grid system pake tile method (kaya di script mygridsystem)
       2. grid system tadi di buat dalam/digabungkan ke fungsi grid manual yg dibuat
       3. dalam gabungan tadi, grid tile method dtambah 0.5f  */
-    // Use this for initialization
+    Tilemap tileMap = null;
+
+    void Awake (){
+        tileMap = transform.GetComponentInParent<Tilemap>();
+    }
     void Start()
     {
+        /*
         GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
         Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
         transform.position = gridLayout.CellToWorld(cellPosition);
+        */
     }
 
     // Update is called once per frame
     void Update () {
 		
 	}
+
+    public Vector3 GetTileCenterPosition(Vector3 position){
+        Vector3Int localPlace;
+        Vector3 centerTile = tileMap.CellToWorld(localPlace);
+        //blom ditambahin 0.5
+        return centerTile;
+    }
 }
