1e72bfa09ba7b03eef437edc6c114d30beeee10c
irgiam <imuqsit@gmail.com>
2018-10-06 18:21:51 +0700

    grid system berhasil di MyGridSystem2 + tambahan bbrp asset
    tambahan bbrp asset tileset buat bangunan

--- Assets/Scripts/ClickableTileTest.cs
+++ Assets/Scripts/ClickableTileTest.cs
@@ -7,13 +7,15 @@ public class ClickableTileTest : MonoBehaviour {
     /*1. buat grid system pake tile method (kaya di script mygridsystem)
       2. grid system tadi di buat dalam/digabungkan ke fungsi grid manual yg dibuat
       3. dalam gabungan tadi, grid tile method dtambah 0.5f  */
-    Tilemap tileMap = null;
+    Tilemap tilemap;
+    [SerializeField] private float size = 1f;
 
     void Awake (){
-        tileMap = transform.GetComponentInParent<Tilemap>();
+        
     }
     void Start()
     {
+        tilemap = transform.GetComponentInParent<Tilemap>();
         /*
         GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
         Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
@@ -27,9 +29,24 @@ public class ClickableTileTest : MonoBehaviour {
 	}
 
     public Vector3 GetTileCenterPosition(Vector3 position){
-        Vector3Int localPlace;
-        Vector3 centerTile = tileMap.CellToWorld(localPlace);
-        //blom ditambahin 0.5
+        Vector3Int localPlace = Vector3Int.zero;
+        Vector3 centerTile = tilemap.CellToWorld(localPlace);
+        centerTile.x += 0.5f;
+        centerTile.y += 0.5f;
         return centerTile;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < tilemap.cellBounds.xMax; x += size)
+        {
+            for (float y = 0; y < tilemap.cellBounds.yMax; y += size)
+            {
+                var point = GetTileCenterPosition(new Vector3(x, y, 0f));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+
+        }
     }
 }
--- Assets/Scripts/MyGridSystem2.cs
+++ Assets/Scripts/MyGridSystem2.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class MyGridSystem2 : MonoBehaviour {
+    Tilemap tileMap = null;
+    public List<Vector3> availablePlaces;
+    // Use this for initialization
+    void Start()
+    {
+        tileMap = transform.GetComponentInParent<Tilemap>();
+        availablePlaces = new List<Vector3>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++)
+        {
+            for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++)
+            {
+                Vector3Int localPlace = (new Vector3Int(n, p, (int)tileMap.transform.position.y));
+                Vector3 place = tileMap.CellToWorld(localPlace);
+                place.x += 0.5f;
+                place.y += 0.5f;
+                if (tileMap.HasTile(localPlace))
+                {
+                    //Tile at "place"
+                    availablePlaces.Add(place);
+                }
+                else
+                {
+                    //No tile at "place"
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < availablePlaces.Count; i++)
+        {
+            Gizmos.DrawSphere(availablePlaces[i], 0.1f);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
