f109e3b99f67be1972a0bc29183f3b7e6ca0f313
irgiam <imuqsit@gmail.com>
2018-10-22 02:50:05 +0700

    scroll tutorial
    part 5

---
+++ Assets/Scripts/ShopScrollList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+    public float price = 1f;
+}
+
+public class ShopScrollList : MonoBehaviour {
+
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public ShopScrollList otherShop;
+    public Text myGoldDisplay;
+    public SimpleObjectPool buttonObjectPool;
+
+    public float gold = 20f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void AddButtons()
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+        }
+    }
+}
