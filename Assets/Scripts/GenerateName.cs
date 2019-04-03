c957c0f5b18226b7abcffb48139149b29694519f
irgiam <imuqsit@gmail.com>
2019-04-03 23:22:44 +0700

    generate random names

---
+++ Assets/Scripts/GenerateName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateName : MonoBehaviour
+{
+    public List<string> names;
+
+    public string[] lines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset nameText = Resources.Load<TextAsset>("SampleName");
+
+        lines = nameText.text.Split("\n"[0]);
+
+        Debug.Log(lines[Random.Range(0, lines.Length)]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
