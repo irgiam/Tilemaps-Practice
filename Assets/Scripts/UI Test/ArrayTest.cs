8655f51fc52283558e87592839086a147b8ea7f4
irgiam <imuqsit@gmail.com>
2019-07-25 17:46:25 +0700

    add array test
    blom fixed

---
+++ Assets/Scripts/UI Test/ArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NameTest
+{
+    Joko,
+    Joni,
+    Bambang,
+    Hendra
+}
+public class ArrayTest : MonoBehaviour
+{
+    public void AddName (NameTest addName, NameTest addName2, NameTest[] test)
+    {
+        test = new NameTest[2] { addName, addName2 };
+    }
+}
