49f71c17acd002bebca5c3621e4de709ca13b851
irgiam <imuqsit@gmail.com>
2019-07-28 15:03:20 +0700

    toggle fixed + screen fix
    scale with screen size (x,y) = rect transform (x,y)

---
+++ Assets/Scripts/UI Test/ToggleControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleControl : MonoBehaviour
+{
+    Toggle thisToggle;
+    public ButtonArrayTest thisButton;
+
+    private void Awake()
+    {
+        thisToggle = GetComponent<Toggle>();
+    }
+
+    public void ToggleChange(bool toggleValue)
+    {
+        int k = 0;
+        for (int i=0; i<thisButton.nameTests.Count; i++)
+        {
+            if (thisButton.nameTests[i].isOn == true)
+            {
+                k += 1;
+            }
+        }
+        if (k > 2)
+        {
+            ToggleOff(thisToggle);
+        }
+    }
+
+    void ToggleOff(Toggle thisToggle)
+    {
+        thisToggle.isOn = false;
+    }
+}
